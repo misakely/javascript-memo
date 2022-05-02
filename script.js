42550a7d5ae774edf647d42e81d2eb307a43ee01
misakely <35529795+misakely@users.noreply.github.com>
2022-05-02 17:39:51 +0300

    Create script.js
    ajout fichier javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+//String
+/*
+    Accéder aux caractères
+
+    Pour accéder aux caractères, on utilise [pos] ou bien la méthode str.CharAt(pos)
+
+*/
+
+let texte = "Hello";
+
+let position = texte.charAt(4);
+
+//console.log(position);
+
+/*
+    Accéder aux caractères
+
+    Les methodes toLowerCase() et toUpperCase() sont deux methodes pour modifier la casse
+
+*/
